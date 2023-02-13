c4fb35e56c844c52b2c60ccf5931c13de5aa5b60
normanlikescats <normankwok97@gmail.com>
2023-02-13 16:22:27 +0800

    fixed random word generator

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,5 @@ import words from "./words.json";
 
 export const getRandomWord = () => {
   // Lowercase words for simplicity
-  // return words[Math.floor(Math.random() * words.length)].toLowerCase();
-  return "guess"
+  return words[Math.floor(Math.random() * words.length)].toLowerCase();
 };
