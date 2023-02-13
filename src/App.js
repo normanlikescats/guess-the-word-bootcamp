73bc950e89dea871c38d526a5e2f5a1062e76c45
normanlikescats <normankwok97@gmail.com>
2023-02-13 16:02:31 +0800

    base code

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class App extends React.Component {
       // guessedLetters stores all letters a user has guessed so far
       guessedLetters: [],
       // Insert num guesses left state here
+      guessesLeft: 10,
       // Insert form input state here
+      currGuess:'',
     };
   }
 
@@ -30,21 +32,68 @@ class App extends React.Component {
   };
 
   // Insert form callback functions handleChange and handleSubmit here
+  handleChange =(e)=>{
+    this.setState({
+      currGuess: e.target.value,
+    });
+  };
+
+  handleSubmit = (e)=>{
+    e.preventDefault();
+    let currGuessesLeft = (this.state.guessesLeft)
+    if(this.state.guessedLetters.indexOf(this.state.currGuess)>-1){
+      alert("💀💀💀 You've already guessed that, ya noob! 💀💀💀")
+    }else if(this.state.currWord.indexOf(this.state.currGuess) === -1){
+      currGuessesLeft -= 1;
+      this.state.guessedLetters.push(this.state.currGuess);
+    } else{
+      this.state.guessedLetters.push(this.state.currGuess);
+    }
+    
+    this.setState({
+      guessedLetters: this.state.guessedLetters,
+      guessesLeft: currGuessesLeft,
+      currGuess: '',
+    })
+  }
+
+  resetGame=()=>{
+    this.setState({
+      currWord: getRandomWord(),
+      guessedLetters: [],
+      guessesLeft: 10,
+      currGuess:'',
+    });
+  }
+
 
   render() {
+    let wordDisplay = this.generateWordDisplay()
+    let output = ""
+    let gameOver = false;
+    if (!wordDisplay.includes("_")){
+      output = `You won!`
+      gameOver = true;
+    } else if(wordDisplay.includes("_") && this.state.guessesLeft === 0 ){
+      output = `You Lost! The word was ${this.state.currWord}!`
+      gameOver = true;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1>Guess The Word 🚀</h1>
           <h3>Word Display</h3>
-          {this.generateWordDisplay()}
-          <h3>Guessed Letters</h3>
-          {this.state.guessedLetters.length > 0
-            ? this.state.guessedLetters.toString()
-            : "-"}
-          <h3>Input</h3>
-          {/* Insert form element here */}
-          Todo: Insert form element here
+          {wordDisplay}
+          <h3>{output}</h3>
+          <h3>Guessed Letters: {this.state.guessedLetters.length > 0
+            ? this.state.guessedLetters.toString(): "-"}</h3>
+          <h3>Guesses Remaining: {this.state.guessesLeft}</h3>
+          {gameOver === false ? <form onSubmit={this.handleSubmit}>
+            <h3>⬇️ Make your guess here ⬇️</h3>
+            <input autoFocus type = 'text' onChange = {this.handleChange} placeholder ='Guess a letter!' value= {this.state.currGuess} />
+            <input type="submit" value="Submit" />
+          </form>: <button onClick = {this.resetGame}>Reset Game!</button>}
         </header>
       </div>
     );
